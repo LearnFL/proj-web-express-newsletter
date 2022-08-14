6498d91e8b02192d30cc1905bc384393af10a3d3
DR <86169204+LearnFL@users.noreply.github.com>
2022-08-14 09:48:58 -0400

    House keeping

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import express from 'express';
 import ejs from 'ejs';
 import https from 'https';
 import path from 'path';
-import client from '@mailchimp/mailchimp_marketing';
+// import client from '@mailchimp/mailchimp_marketing';
 import {} from 'dotenv/config';
 
 // APP SET UP
@@ -53,10 +53,9 @@ app.post('/', (req, res)=>{
   const request = https.request(url, options, (resp) => {
                     console.log('statusCode:', resp.statusCode);
                     resp.on('data', (d) => {
-                      console.log(JSON.parse(d));
+                      // console.log(JSON.parse(d));
                       var subscriber_status = JSON.parse(d);
-                      console.log(" received dataaaa");
-                      console.log(subscriber_status);
+                      // console.log(subscriber_status);
                       if(resp.statusCode == 200 && subscriber_status.total_created == 1) {
                         res.render('success');
                       } else {
